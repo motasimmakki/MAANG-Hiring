0eeb60f622e63599e8dad050039af39a6ba67cb1
motasimmakki <makkimotasim@gmail.com>
2022-12-17 02:27:42 +0530

    :zap: feat: Partial structure of extension as extension

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import './components/Navbar.css';
+import Filter from './components/Filter';
+import './components/Filter.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Filter/>
     </div>
   );
 }
