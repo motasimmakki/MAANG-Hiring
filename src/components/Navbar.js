7fffe877bada4c00e6e29b6970ddea49ebeab92d
motasimmakki <makkimotasim@gmail.com>
2022-12-17 03:21:13 +0530

    :art: feat: Improved Navbar [:bug:]

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import maang from '../images/maang.png';
 
 export default function Navbar() {
   return (
-    <div className='navbar-cont'>
-        Navbar
-    </div>
+      <AppBar position="static" style={{ background: '#3e5175' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters className='navbar-cont'>
+        <img className='navbar-logo' src={maang} alt="logo"/>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            HIRING
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
