2ffafaa71db301069da826c9fdfebf945d10a498
motasimmakki <makkimotasim@gmail.com>
2022-12-25 12:08:34 +0530

    :art: chore: Altered structure dimensions

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Navbar from './components/Navbar';
 import './components/Navbar.css';
 import Filter from './components/Filter';
 import './components/Filter.css';
+import List from './components/List';
+import './components/List.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Filter/>
+      <List/>
     </div>
   );
 }
