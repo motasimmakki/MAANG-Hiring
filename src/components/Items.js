586cf6de3a66c4b57711fe06472d919a552a3f32
motasimmakki <makkimotasim@gmail.com>
2023-03-28 16:18:00 +0530

    :technologist:feat: Map over each job to list:zap:

--- src/components/Items.js
+++ src/components/Items.js
@@ -7,44 +7,33 @@ export default function Items() {
     } else {
         // NOT A SINGLE JOB AVAILABLE!
     }
-
     return (
         <div className='items-cont'>
-            {/* {
-                data.map((company) => {
-
-                })
-            } */}
-            <div className='item'>
-
-            </div>
-            <div className='item'>
-
-            </div>
-            <div className='item'>
-
-            </div>
-            <div className='item'>
-
-            </div>
-            <div className='item'>
-
-            </div>
-            <div className='item'>
-
-            </div>
-            <div className='item'>
-
-            </div>
-            <div className='item'>
-
-            </div>
-            <div className='item'>
-
-            </div>
-            <div className='item'>
-
-            </div>
+            {
+            Object.keys(data).map((company, idx) => (
+                data[company].job_link.map((link, idx) => (
+                    <div className='item' key={idx}>
+                        <a className='item-link' href={link} target='_blank' rel='noreferrer' key={idx}>
+                            <div className='single-item-cont'>
+                                <h3 className='item-title'>{data[company].job_titles.at(idx)}</h3>
+                                <h3 className='item-posting'>{data[company].job_posting.at(idx)}</h3>
+                                <h3 className='item-location'>{data[company].location_and_id.at(idx)}</h3>
+                            </div>        
+                        </a>
+                    </div>
+                ))
+            ))
+            }
+            {/* <div className='item'>
+                <a className='item-link' href={data.amazon.job_link.at(0)} target='_blank' rel='noreferrer'>
+                    <div className='single-item-cont'>
+                        <h3 className='item-title'>{data.amazon.job_titles.at(0)}</h3>
+                        <h3 className='item-posting'>{data.amazon.job_posting.at(0)}</h3>
+                        <h3 className='item-location'>{data.amazon.location_and_id.at(0)}</h3>
+                    </div>
+                </a>
+            </div> */}
+            
         </div>
     )
 }
