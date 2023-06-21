bdee3e9f75156a313114643711553df92b5464e5
motasimmakki <makkimotasim@gmail.com>
2023-06-22 01:20:26 +0530

    :technologist:Feat: Save interested job in LocalStorage, Added animation over bookmark icon:art::zap:

--- src/components/Items.js
+++ src/components/Items.js
@@ -34,23 +34,48 @@ export default function Items({ data, themeMode }) {
         setIsLoading(true);
         waitForAWhile(1000).then(() => setIsLoading(false));
     }, [data]);
+    const [interestList, updateInterestedList] = useState({});
+    function addToInterested (company, idx, item) {
+        // console.log(item);
+        item.classList.add("scale-click");
+        // console.log(company + " - " + idx);
+        let key = company + "-" + idx;
+        if(!(key in interestList)) {
+            let postObj = {
+                job_title: data[company].job_title[idx],
+                job_location: data[company].job_location[idx],
+                job_link: data[company].job_link[idx]
+            }
+            let newInterestedList = {};
+            for(let key in interestList) {
+                newInterestedList[key] = interestList[key];
+            }
+            newInterestedList[key] = postObj;
+            updateInterestedList(newInterestedList);
+        }
+    }
+    useEffect(() => {
+        // Getting updated interestList data.
+        // console.log(interestList);
+        localStorage.setItem("interestedJobs", JSON.stringify(interestList));
+    }, [interestList]);
     return (
         (isLoading) ? null :
             // For change Theme.
             (themeMode === "light")?
-                (<div className='items-cont light' id="items-cont">
+                (<div className='items-cont light'>
                     {
                     Object.keys(data).map((company, idx) => (
                         data[company].job_link.map((link, idx) => (
-                            <div className='item light' id="item" key={idx}>
+                            <div className='item light' key={idx}>
                                 <a className='item-link' href={link} target='_blank' rel='noreferrer' key={idx}>
-                                    <div className='single-item-cont light' id="single-item">
+                                    <div className='single-item-cont light'>
                                         {
                                         (data[company].job_posting) ?
-                                            <h3 className='item-posting light' id="item-posting">{data[company].job_posting.at(idx)}</h3>
-                                            : <h3 className='item-posting light' id="item-posting"><strong>_ _ _</strong></h3>
+                                            <h3 className='item-posting light'>{data[company].job_posting.at(idx)}</h3>
+                                            : <h3 className='item-posting light'><strong>_ _ _</strong></h3>
                                         }
-                                        <h3 className='item-title light' id="item-title">
+                                        <h3 className='item-title light'>
                                             <img src={logoPaths[company]} className='company-icon' alt={company + '-logo'}></img>
                                             {
                                             ((data[company].job_title.at(idx).length) > 40) ?
@@ -58,15 +83,24 @@ export default function Items({ data, themeMode }) {
                                                 : data[company].job_title.at(idx)
                                             }
                                         </h3>
-                                        <h3 className='item-location light' id="item-location">{data[company].job_location.at(idx)}</h3>
+                                        <h3 className='item-location light'>{data[company].job_location.at(idx)}</h3>
                                     </div>
                                 </a>
     {/*-------------------------------  ADD to Favorite Button and SVG------------------------------------------------  */}
-                                <button class="add-to-favorites light" id="mark-fav">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon">
-                                        <path d="M5 3h14a2 2 0 0 1 2 2v16l-8-4-8 4V5a2 2 0 0 1 2-2z"></path>
-                                    </svg>
-                                </button>
+                                {
+                                (interestList.hasOwnProperty(company + "-" + idx))?
+                                    (<button class="add-to-favorites light">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="green" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon">
+                                            <path d="M5 3h14a2 2 0 0 1 2 2v16l-8-4-8 4V5a2 2 0 0 1 2-2z"></path>
+                                        </svg>
+                                    </button>)
+                                :
+                                    (<button class="add-to-favorites light">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon" onClick={(event) => addToInterested(company, idx, event.currentTarget)}>
+                                            <path d="M5 3h14a2 2 0 0 1 2 2v16l-8-4-8 4V5a2 2 0 0 1 2-2z"></path>
+                                        </svg>
+                                    </button>)
+                                }
                             </div>
                         ))
                     ))
