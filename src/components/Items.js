ee4d6ae651317e152fc8f24ceb8aa70fe93a105d
motasimmakki <makkimotasim@gmail.com>
2023-06-22 01:25:31 +0530

    :rocket:chore: Added same functionality for dark mode:fire:

--- src/components/Items.js
+++ src/components/Items.js
@@ -131,11 +131,20 @@ export default function Items({ data, themeMode }) {
                                     </div>
                                 </a>
         {/* -------------------------------  ADD to Favorite Button and SVG------------------------------------------------  */}
-                                <button class="add-to-favorites">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon">
-                                        <path d="M5 3h14a2 2 0 0 1 2 2v16l-8-4-8 4V5a2 2 0 0 1 2-2z"></path>
-                                    </svg>
-                                </button>
+                                {
+                                (interestList.hasOwnProperty(company + "-" + idx))?
+                                    (<button class="add-to-favorites">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="green" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon">
+                                            <path d="M5 3h14a2 2 0 0 1 2 2v16l-8-4-8 4V5a2 2 0 0 1 2-2z"></path>
+                                        </svg>
+                                    </button>)
+                                :
+                                    (<button class="add-to-favorites">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon" onClick={(event) => addToInterested(company, idx, event.currentTarget)}>
+                                            <path d="M5 3h14a2 2 0 0 1 2 2v16l-8-4-8 4V5a2 2 0 0 1 2-2z"></path>
+                                        </svg>
+                                    </button>)
+                                }
                             </div>
                         ))
                     ))
