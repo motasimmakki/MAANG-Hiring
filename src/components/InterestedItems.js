f2730b704f6ccf0c0c6290ff8d68ed62503705c4
motasimmakki <makkimotasim@gmail.com>
2023-06-22 14:27:43 +0530

    :art:style: fixed styling of active & deActive tabs

--- src/components/InterestedItems.js
+++ src/components/InterestedItems.js
@@ -21,7 +21,7 @@ export default function InterestedItems() {
     const [interestList, updateInterestedList] = useState(JSON.parse(localStorage.getItem("interestedJobs")));
     return (
         <div className='items-cont light'>
-            {
+            {   interestList &&
                 Object.keys(interestList).map((company, idx) => (
                     <div className='item light'>
                         <a className='item-link' href={interestList[company].job_link} target='_blank' rel='noreferrer'>
@@ -48,22 +48,6 @@ export default function InterestedItems() {
                                 <path d="M6 18a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v11zM19 3h-3L16 2H8L7 3H4v2h15V3z" />
                             </svg>
                         </button>
-
-                        {/*-------------------------------  ADD to Favorite Button and SVG------------------------------------------------  */}
-                        {/* {
-                (interestList?.hasOwnProperty(company + "-" + idx))?
-                    (<button class="add-to-favorites light">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="green" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon">
-                            <path d="M5 3h14a2 2 0 0 1 2 2v16l-8-4-8 4V5a2 2 0 0 1 2-2z"></path>
-                        </svg>
-                    </button>)
-                :
-                    (<button class="add-to-favorites light">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon" onClick={(event) => addToInterested(company, idx, event.currentTarget)}>
-                            <path d="M5 3h14a2 2 0 0 1 2 2v16l-8-4-8 4V5a2 2 0 0 1 2-2z"></path>
-                        </svg>
-                    </button>)
-                } */}
                     </div>
                 ))
             }
--- src/components/List.js
+++ src/components/List.js
@@ -25,10 +25,22 @@ export default function List({ data, filterStatus, themeMode }) {
   const [currTab, changeCurrTab] = useState("all-jobs");
   function changeTab(currTab) {
     // console.log(currTab)
+    let all_btn = document.getElementById('all-btn');
+    let fav_btn = document.getElementById('fav-btn');
     if(currTab.classList.contains('btn-all')) {
-      changeCurrTab("all-jobs")
+      changeCurrTab("all-jobs");
+      all_btn.classList.remove('deActive-tab');
+      all_btn.classList.add('active-tab');
+      
+      fav_btn.classList.remove('active-tab');
+      fav_btn.classList.add('deActive-tab');
     } else {
-      changeCurrTab("interested-jobs")
+      changeCurrTab("interested-jobs");
+      all_btn.classList.remove('active-tab');
+      all_btn.classList.add('deActive-tab');
+      
+      fav_btn.classList.remove('deActive-tab');
+      fav_btn.classList.add('active-tab');
     }
   }
   // useEffect(() => {
@@ -39,7 +51,7 @@ export default function List({ data, filterStatus, themeMode }) {
     <div className='list-cont light'id="list-cont">
       {/* ----------------------------------tab buttons added below------------- */}
       <div className='tabs light' id="tabs">
-        <button className='btn btn-all light' id="all-btn" onClick={(event) => changeTab(event.currentTarget)}>All Jobs</button>
+        <button className='btn btn-all light active-tab' id="all-btn" onClick={(event) => changeTab(event.currentTarget)}>All Jobs</button>
         <button className='btn btn-favorite deActive-tab light' id="fav-btn" onClick={(event) => changeTab(event.currentTarget)}>Interested</button>
       </div>
       <div className='tab-view'>
