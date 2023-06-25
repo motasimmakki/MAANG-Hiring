d67aeaf278c85c7913a11e51502b05f48670ccdb
motasimmakki <makkimotasim@gmail.com>
2023-06-25 18:36:19 +0530

    :technologist:feat: Implemented the backend for tab-curves adjustments:zap:

--- src/components/List.js
+++ src/components/List.js
@@ -11,7 +11,7 @@ export default function List({ data, themeMode }) {
     const tabs_classes = document.getElementById('tabs').classList;
     const all_btn_classes = document.getElementById('all-btn').classList;
     const fav_btn_classes = document.getElementById('fav-btn').classList;
-    const curve_classes=document.getElementById('curve').classList;
+    const curve_classes = document.getElementById('curve').classList;
     if (themeMode === "light") {
       listCont_classes.add('light');
       tabs_classes.add('light');
@@ -31,6 +31,8 @@ export default function List({ data, themeMode }) {
     // console.log(currTab)
     let all_btn = document.getElementById('all-btn');
     let fav_btn = document.getElementById('fav-btn');
+    const div = document.createElement('div');
+    div.id = 'curve';
     if (currTab.classList.contains('btn-all')) {
       changeCurrTab("all-jobs");
       all_btn.classList.remove('deActive-tab');
@@ -38,6 +40,16 @@ export default function List({ data, themeMode }) {
 
       fav_btn.classList.remove('active-tab');
       fav_btn.classList.add('deActive-tab');
+
+      all_btn.innerHTML = "";
+      if(themeMode === 'light') {
+        div.className = 'border-outer-all-jobs light';
+      } else {
+        div.className = 'border-outer-all-jobs';
+      }
+      all_btn.appendChild(div);
+      all_btn.innerHTML += 'Available';
+      fav_btn.innerHTML = "Interested";
     } else {
       changeCurrTab("interested-jobs");
       all_btn.classList.remove('active-tab');
@@ -45,6 +57,16 @@ export default function List({ data, themeMode }) {
 
       fav_btn.classList.remove('deActive-tab');
       fav_btn.classList.add('active-tab');
+
+      all_btn.innerHTML = "Available";
+      fav_btn.innerHTML = "";
+      if(themeMode === 'light') {
+        div.className = 'border-outer-interested light';
+      } else {
+        div.className = 'border-outer-interested';
+      }
+      fav_btn.appendChild(div);
+      fav_btn.innerHTML += 'Interested';
     }
   }
   // useEffect(() => {
@@ -56,9 +78,13 @@ export default function List({ data, themeMode }) {
       {/* -------------------------tab buttons added below--------------------- */}
       <div className='tabs light' id="tabs">
         <button className='btn btn-all light active-tab' id="all-btn" onClick={(event) => changeTab(event.currentTarget)}>
-          <div className='border-outer-all-jobs light' id='curve'></div>Available</button>
+          <div className='border-outer-all-jobs light' id='curve'></div>
+          Available
+        </button>
         <button className='btn btn-favorite deActive-tab light' id="fav-btn" onClick={(event) => changeTab(event.currentTarget)}>
-          <div className='border-outer-interested light' id='curve'></div>Interested</button>
+          {/* <div className='border-outer-interested light' id='curve'></div> */}
+          Interested
+        </button>
       </div>
       <div className='tab-view'>
         <div className='loader'>
